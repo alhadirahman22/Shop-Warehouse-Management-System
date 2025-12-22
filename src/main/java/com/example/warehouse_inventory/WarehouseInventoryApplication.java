826eb9ab51ab9ce27a09846dca5fc093d744fe55
Alhadi Rahman <alhadirahman22@gmail.com>
2025-12-22 13:41:03 +0700

    feat: :sparkles: first

---
+++ src/main/java/com/example/warehouse_inventory/WarehouseInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.warehouse_inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/warehouse_inventory/WarehouseInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.warehouse_inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
