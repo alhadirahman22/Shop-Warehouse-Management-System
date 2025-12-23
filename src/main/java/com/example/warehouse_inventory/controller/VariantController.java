cf9fd47ace85187e039a3c5efb987db5f25cdcb7
Alhadi Rahman <alhadirahman22@gmail.com>
2025-12-23 09:20:31 +0700

    feat: :sparkles: variant first

---
+++ src/main/java/com/example/warehouse_inventory/controller/VariantController.java
@@ -0,0 +1,13 @@
+package com.example.warehouse_inventory.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RequestMapping("/variant")
+@RestController
+@RequiredArgsConstructor
+public class VariantController {
+
+}
